9d09abe8af737f0ca4e1e69720c5c8702ab41502
25gillp <25gillp@retsdstudent.ca>
2025-02-06 17:22:41 +0000

    finished

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -20,11 +20,16 @@ public class Assignment {
     public Assignment() {
         // Students will initialize their variables here
         // Example (students should change these values):
-        myString = null;
-        myInt = 0;
-        myDouble = 0.0;
-        myBoolean = false;
-        myStringArray = null;
-        myIntArray = null;
+        myString = "Java String";
+        myInt = 47;
+        myDouble = 89.23;
+        myBoolean = true;
+        myStringArray = new String[] {
+            "first", "second", "third"
+        };
+
+        myIntArray = new int[] {
+            1, 2, 3
+        };
     }
 }
